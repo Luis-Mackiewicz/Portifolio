8c861df46caccef6c3787e6bba401ebe849a2ad6
luis mackiewicz <luismiguelpix@gmail.com>
2025-12-10 17:11:25 -0300

    feat(home): add id

--- app/page.tsx
+++ app/page.tsx
@@ -2,22 +2,11 @@ import Header from "./components/header";
 import Image from "next/image";
 export default function Home() {
   return (
-    <div className="bg-black w-full h-screen">
-      <Header />
-      <main className=" flex flex-col items-center text-white w-full h-max pt-9">
-        <div className="bg-linear-to-r from-(--primary) to-(--terciary) bg-clip-text text-transparent">
-          <h1 className="font-serif text-lg">
-            Luis Miguel Oliveira de Almeida Mackiewicz
-          </h1>
-        </div>
-        <h2 className="font-sans text-base">Desenvolvedor Fullstack</h2>
-        <Image
-          src="/public/luis-ilustracao.avif"
-          alt="my image"
-          width={500}
-          height={500}
-        />
-      </main>
-    </div>
+    <main>
+      <section id="inicio"></section>
+      <section id="portifolio"></section>
+      <section id="sobre"></section>
+      <section id="contato"></section>
+    </main>
   );
 }
