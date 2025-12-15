41404aec75d7ed1fe026c118ca1dd5168007c47f
luis mackiewicz <luismiguelpix@gmail.com>
2025-12-15 17:16:18 -0300

    feat(page): add texts, basic styling to the "about" and "contact" sections

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Header from "./components/header";
 import Image from "next/image";
 import CircleBackground from "@/public/circle-background.svg";
 import LuisIlustration from "@/public/luis-ilustration.svg";
+import LuisImage from "@/public/luis-miguel.svg";
 import WhatsappIcon from "@/public/icons/whatsap.svg";
 import Button from "./components/button";
 
@@ -11,7 +12,7 @@ export default function Home() {
       <Header />
       <section
         id="inicio"
-        className="  flex flex-col w-full h-screen mt-36 items-center border-b border-white"
+        className="flex flex-col w-full h-screen mt-36 items-center border-b border-white"
       >
         <div className="w-full flex items-center justify-between">
           <div className="w-1/2 flex flex-col items-center">
@@ -60,9 +61,69 @@ export default function Home() {
       <section
         id="portifolio"
         className="w-full h-screen border-b border-white"
-      ></section>
-      <section id="sobre" className="w-full h-screen "></section>
-      <section id="contato" className="w-full h-screen bg-terciary"></section>
+      >
+        <h2>Portifólio</h2>
+        <p>Em breve!</p>
+      </section>
+      <section id="sobre" className="w-full h-screen flex items-center">
+        <div className="flex flex-col justify-between">
+          <h2 className="font-cinzel font-bold text-6xl">Sobre</h2>
+          <Image
+            src={LuisImage}
+            alt="Luis Miguel playing guitar"
+            width={500}
+            height={500}
+            className="border-4"
+          />
+        </div>
+        <div className="flex flex-col w-1/2">
+          <p>
+            Olá, eu sou o Luis Miguel Oliveira de Almeida Mackiewicz.
+            Desenvolvedor fullstack TypeScript, com experiência complementar em
+            design e marketing digital. Minha jornada na tecnologia começou em
+            2024, quando ingressei no curso de Análise e Desenvolvimento de
+            Sistemas. No mesmo ano, conquistei meu primeiro estágio na área,
+            onde mergulhei de vez no desenvolvimento de software. Em 2025, atuei
+            com marketing digital, trabalhando com ADS e criação de conteúdo
+            para plataformas como Instagram, Facebook, Google e TikTok —
+            experiência que hoje utilizo para criar soluções que não só
+            funcionam, mas que também geram resultado. Atualmente atuo como
+            desenvolvedor freelancer, ajudando empresas e profissionais a irem
+            além de apenas “ter um site”. Meu foco é construir páginas
+            estratégicas, pensadas para atrair clientes, aumentar vendas e
+            fortalecer a identidade visual da sua marca. Se você busca alguém
+            que una tecnologia, design e estratégia comercial, estou pronto para
+            transformar sua ideia em um projeto de impacto.
+          </p>
+          <div className="flex">
+            <Button active className="mt-20">
+              Linkedin
+            </Button>
+            <Button active className="mt-20">
+              Chame
+              <Image
+                src={WhatsappIcon}
+                width={36}
+                height={36}
+                alt="Whatsapp icon"
+              ></Image>
+            </Button>
+          </div>
+        </div>
+      </section>
+      <section
+        id="contato"
+        className="w-full h-screen flex flex-col items-center justify-center bg-secundary"
+      >
+        <h2 className="font-cinzel font-bold text-6xl">
+          Vamos transformar sua idéia em realidade!
+        </h2>
+        <h3>
+          Desde o conceito até o resultado final, meu foco é impulsionar o seu
+          negócio
+        </h3>
+        <Button active>Entrar em contato</Button>
+      </section>
     </main>
   );
 }
