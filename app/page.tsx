9e06fa040ec71bad2c3580d8fc2c4e60e4b6c18a
luis mackiewicz <luismiguelpix@gmail.com>
2025-12-15 10:19:24 -0300

    feat(page): add home text, fonts and text size

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,15 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <section id="inicio"></section>
+      <section id="inicio">
+        <h1 className="text-6xl font-bold font-cinzel bg-linear-to-l from-(--degrade) to-(--terciary) bg-clip-text text-transparent">
+          Luis Miguel Mackiewicz
+        </h1>
+        <p>
+          Conquiste mais clientes com um site focado em conversões e destaque
+          sua marca com uma identidade visual poderosa
+        </p>
+      </section>
       <section id="portifolio"></section>
       <section id="sobre"></section>
       <section id="contato"></section>
