fdbb7af29f5ee98ecfa7290d814b316d91d54432
luis mackiewicz <luismiguelpix@gmail.com>
2026-01-15 15:40:18 -0300

    feat(fonts): add font lobster two

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,4 @@
-import { Cinzel, Inter } from "next/font/google";
+import { Lobster_Two, Cinzel, Inter } from "next/font/google";
 
 export const cinzel = Cinzel({
   variable: "--font-cinzel",
@@ -9,6 +9,13 @@ export const cinzel = Cinzel({
 export const inter = Inter({
   variable: "--font-inter",
   weight: ["400", "700", "900"],
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const lobsterTwo = Lobster_Two({
+  variable: "--font-lobster",
+  weight: ["400", "700"],
   subsets: ["latin"],
   display: "swap",
 });
