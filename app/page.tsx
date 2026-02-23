785453d3279f8979f696625b3096d7f74377aff5
luis mackiewicz <luismiguelpix@gmail.com>
2026-02-23 19:09:04 -0300

    feat(home): adjusted layout

--- app/page.tsx
+++ app/page.tsx
@@ -4,23 +4,23 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <>
+    <div>
       <Header />
-
-      <section
-        id="inicio"
-        className="relative flex w-full items-center justify-center bg-black overflow-hidden pt-24 h-screen"
-      >
-        <video
-          autoPlay
-          muted
-          loop
-          playsInline
-          preload="auto"
-          controls={false}
-          disablePictureInPicture
-          disableRemotePlayback
-          className="
+      <div className="h-screen overflow-y-scroll snap-y snap-mandatory ">
+        <section
+          id="inicio"
+          className="relative flex w-full items-center justify-center bg-black overflow-hidden  h-screen snap-start"
+        >
+          <video
+            autoPlay
+            muted
+            loop
+            playsInline
+            preload="auto"
+            controls={false}
+            disablePictureInPicture
+            disableRemotePlayback
+            className="
             h-[calc(100vh-8rem)]
             w-[90vw]
             max-w-none
@@ -30,63 +30,89 @@ export default function Home() {
             select-none
             opacity-90
           "
+          >
+            <source src="/coding-video.mp4" type="video/mp4" />
+          </video>
+
+          <div className="absolute flex flex-col gap-4 items-center  text-center text-white">
+            <h1 className="text-6xl font-barbra font-bold">
+              Olá, eu sou Luis Miguel Mackiewicz
+            </h1>
+            <h2 className="text-xl  text-zinc-300">
+              Desenvolvedor de software
+            </h2>
+          </div>
+        </section>
+
+        <section
+          id="sobre"
+          className="w-full bg-black px-6 py-24 text-white snap-start "
         >
-          <source src="/coding-video.mp4" type="video/mp4" />
-        </video>
+          <div className="mx-auto max-w-6xl">
+            <h2 className="font-barbra text-4xl md:text-5xl mb-12">Sobre</h2>
 
-        <div className="absolute flex flex-col gap-4 items-center  text-center text-white">
-          <h1 className="text-6xl font-barbra font-bold">
-            Olá, eu sou Luis Miguel Mackiewicz
-          </h1>
-          <h2 className="text-xl  text-zinc-300">Desenvolvedor de software</h2>
-        </div>
-      </section>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-80 items-center">
+              <div className="flex justify-center">
+                <Image
+                  src={MyselfImage}
+                  alt="Luis Miguel tocando guitarra"
+                  width={420}
+                  className="rounded-3xl object-cover grayscale hover:grayscale-0 transition duration-500"
+                />
+              </div>
 
-      <section id="sobre" className="w-full bg-black px-6 py-24 text-white">
-        <div className="mx-auto max-w-6xl">
-          <h2 className="font-barbra text-4xl md:text-5xl mb-12">Sobre</h2>
+              <div className="space-y-5 text-zinc-300 leading-relaxed ">
+                <p>
+                  Meu nome é Luis Miguel Mackiewicz , desenvolvedor fullstack
+                  TypeScript, com experiência complementar em design e marketing
+                  digital.
+                </p>
 
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-80 items-center">
-            <div className="flex justify-center">
-              <Image
-                src={MyselfImage}
-                alt="Luis Miguel tocando guitarra"
-                width={420}
-                className="rounded-3xl object-cover grayscale hover:grayscale-0 transition duration-500"
-              />
-            </div>
+                <p>
+                  Iniciei minha jornada na tecnologia em 2024, ao ingressar no
+                  curso de Análise e Desenvolvimento de Sistemas, e no mesmo ano
+                  conquistei meu primeiro estágio, onde embarquei no
+                  desenvolvimento de software.
+                </p>
 
-            <div className="space-y-5 text-zinc-300 leading-relaxed ">
-              <p>
-                Meu nome é Luis Miguel Mackiewicz , desenvolvedor fullstack
-                TypeScript, com experiência complementar em design e marketing
-                digital.
-              </p>
+                <p>
+                  Em 2025, atuei com marketing digital, trabalhando com ADS e
+                  criação de conteúdo para plataformas como Instagram, Facebook,
+                  Google e TikTok — experiência que hoje aplico para criar
+                  produtos que não apenas funcionam, mas convertem.
+                </p>
 
-              <p>
-                Iniciei minha jornada na tecnologia em 2024, ao ingressar no
-                curso de Análise e Desenvolvimento de Sistemas, e no mesmo ano
-                conquistei meu primeiro estágio, onde embarquei no
-                desenvolvimento de software.
-              </p>
-
-              <p>
-                Em 2025, atuei com marketing digital, trabalhando com ADS e
-                criação de conteúdo para plataformas como Instagram, Facebook,
-                Google e TikTok — experiência que hoje aplico para criar
-                produtos que não apenas funcionam, mas convertem.
-              </p>
-
-              <p>
-                Atualmente atuo como desenvolvedor freelancer , ajudando
-                empresas e profissionais a irem além de apenas “ter um site”.
-                Meu foco é construir páginas estratégicas, pensadas para atrair
-                clientes, aumentar vendas e fortalecer marcas.
-              </p>
+                <p>
+                  Atualmente atuo como desenvolvedor freelancer , ajudando
+                  empresas e profissionais a irem além de apenas “ter um site”.
+                  Meu foco é construir páginas estratégicas, pensadas para
+                  atrair clientes, aumentar vendas e fortalecer marcas.
+                </p>
+              </div>
             </div>
           </div>
-        </div>
-      </section>
-    </>
+        </section>
+        <section
+          id="portifolio"
+          className="w-full h-screen flex justify-center items-center bg-black snap-start"
+        >
+          <h2 className=" text-8xl text-white font-barbra">Em breve!</h2>
+        </section>
+        <section
+          id="contato"
+          className="w-full h-screen flex flex-col items-center bg-linear-to-b from-sky-600 to-emerald-600 snap-start"
+        >
+          <div className="flex-1 flex items-center justify-center px-6 text-center">
+            <h2 className="text-white text-6xl font-barbra">
+              Vamos transformar sua ideia em realidade!
+            </h2>
+          </div>
+
+          <footer className="mb-8 text-white text-sm">
+            © 2025 Luis Miguel Mackiewicz. All rights reserved.
+          </footer>
+        </section>
+      </div>
+    </div>
   );
 }
