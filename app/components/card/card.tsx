f6002df6ea3c9fc763701d8c1f680a9c52448830
luis mackiewicz <luismiguelpix@gmail.com>
2026-02-25 12:06:21 -0300

    feat: add new components

---
+++ app/components/card/card.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { useState } from "react";
+
+const [count, setCount] = useState();
+
+export interface cardProps {
+  name: string;
+  src: string;
+  className: string;
+  onclick: () => void;
+  children: React.ReactNode;
+}
+
+export default function Card({
+  name,
+  src,
+  className,
+  onclick,
+  children,
+}: cardProps) {
+  return <div></div>;
+}
