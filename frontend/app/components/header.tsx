474b82048670f7735f0a119b9548f9b51208bd88
luis mackiewicz <luismiguelpix@gmail.com>
2026-02-26 12:57:11 -0300

    style(header): add responsivness

--- frontend/app/components/header.tsx
+++ frontend/app/components/header.tsx
@@ -8,31 +8,31 @@ export default function Header() {
         top-0
         z-50
         w-full
-        bg-black/60
+        bg-black/5
         backdrop-blur-md
         border-b
         border-white/10
       "
     >
-      <nav className="mx-auto max-w-6xl px-6 py-3">
-        <ul className="flex items-center justify-between text-sm font-jet font-medium text-white">
-          <li className="opacity-90 hover:opacity-50 transition cursor-target">
+      <nav className=" h-12 w-full flex justify-center">
+        <ul className="w-full flex items-center justify-between gap-2 text-xs font-jet font-bold text-white sm:w-4/5 md:text-base">
+          <li className="opacity-80 hover:opacity-50 transition cursor-target">
             <Link href="#inicio">início</Link>
           </li>
 
-          <li className="opacity-90 hover:opacity-50 transition cursor-target">
+          <li className="opacity-80 hover:opacity-50 transition cursor-target">
             <Link href="#sobre">sobre</Link>
           </li>
 
-          <li className="opacity-90 hover:opacity-50 transition cursor-target">
+          <li className="opacity-80 hover:opacity-50 transition cursor-target">
             <Link href="#portifolio">portifólio</Link>
           </li>
 
-          <li className="opacity-90 hover:opacity-50 transition cursor-target">
+          <li className="opacity-80 hover:opacity-50 transition cursor-target">
             <Link href="#habilidades">habilidades</Link>
           </li>
 
-          <li className="opacity-90 hover:opacity-50 transition cursor-target">
+          <li className="opacity-80 hover:opacity-50 transition cursor-target">
             <Link href="#contato">contato</Link>
           </li>
         </ul>
