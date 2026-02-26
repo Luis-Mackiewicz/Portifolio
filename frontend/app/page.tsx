b3aed900949e6d3a656d376ea57d90053b4022f0
luis mackiewicz <luismiguelpix@gmail.com>
2026-02-26 20:50:11 -0300

    style: changing colors and sizes

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 import { useEffect } from "react";
 import Button from "./components/button";
 import Card, { technologyCards, websiteCards } from "./components/card";
+import { Video } from "./components/video";
 export default function Home() {
   const consoleText = `
   ██╗     ██╗   ██╗██╗███████╗    ███╗   ███╗ █████╗  ██████╗██╗  ██╗██╗███████╗██╗    ██╗██╗ ██████╗███████╗
@@ -41,34 +42,12 @@ export default function Home() {
     h-screen
     items-center
     justify-center
-    bg-black
+    bg-linear-to-r from-gray-700 via-gray-900 to-black
     overflow-hidden
     snap-start
   "
         >
-          <video
-            autoPlay
-            muted
-            loop
-            playsInline
-            preload="auto"
-            controls={false}
-            disablePictureInPicture
-            disableRemotePlayback
-            className="
-            
-      h-[70%]
-      w-[80%]
-      max-w-none
-      rounded-3xl
-      object-cover
-      pointer-events-none
-      select-none
-      opacity-60
-    "
-          >
-            <source src="/coding-video.mp4" type="video/mp4" />
-          </video>
+          <Video />
 
           <div
             className="
@@ -86,7 +65,7 @@ export default function Home() {
             <div className="space-y-4">
               <h1
                 className="
-          text-4xl
+          text-xl
           md:text-4xl
           font-jet
           font-bold
@@ -99,8 +78,10 @@ export default function Home() {
               <h2
                 className="
           text-lg
+          font-bold
+
           md:text-xl
-          text-zinc-300
+          text-white/50
           tracking-wide
         "
               >
@@ -108,7 +89,7 @@ export default function Home() {
               </h2>
             </div>
 
-            <div className="pt-4">
+            <div className="w-full">
               <Button>contato</Button>
             </div>
           </div>
@@ -116,58 +97,59 @@ export default function Home() {
 
         <section
           id="sobre"
-          className="w-full bg-black px-6 py-24 text-white snap-start "
+          className="w-full h-screen flex flex-col justify-center items-center text-white
+              bg-linear-to-r from-gray-700 via-gray-900 to-black
+
+          snap-start "
         >
-          <div className="mx-auto max-w-6xl">
-            <h2 className="font-jet font-bold text-4xl md:text-5xl mb-12">
-              Sobre
-            </h2>
+          <h2 className="font-jet font-bold text-4xl md:text-5xl mb-12">
+            Sobre
+          </h2>
 
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-80 items-center">
-              <div className="flex justify-center">
-                <Image
-                  src={MyselfImage}
-                  alt="Luis Miguel tocando guitarra"
-                  width={420}
-                  sizes="(max-width: 768px) 100vw, 420px"
-                  className="rounded-3xl object-cover grayscale hover:grayscale-0 transition duration-500 cursor-target"
-                />
-              </div>
+          <div className="w-4/5 grid grid-cols-1 md:grid-cols-2 items-center">
+            <div>
+              <Image
+                src={MyselfImage}
+                alt="Imagem de Luis Miguel tocando violão"
+                width={500}
+                className="rounded-4xl object-cover grayscale hover:grayscale-0 transition duration-500 cursor-target"
+              />
+            </div>
 
-              <div className="space-y-5 text-zinc-300 leading-relaxed ">
-                <p>
-                  Meu nome é Luis Miguel Mackiewicz , desenvolvedor fullstack
-                  TypeScript, com experiência complementar em design e marketing
-                  digital.
-                </p>
+            <div className="text-xs space-y-3 text-white">
+              <p>
+                Meu nome é Luis Miguel Mackiewicz , desenvolvedor fullstack
+                TypeScript, com experiência complementar em design e marketing
+                digital.
+              </p>
 
-                <p>
-                  Iniciei minha jornada na tecnologia em 2024, ao ingressar no
-                  curso de Análise e Desenvolvimento de Sistemas, e no mesmo ano
-                  conquistei meu primeiro estágio, onde embarquei no
-                  desenvolvimento de software.
-                </p>
+              <p>
+                Iniciei minha jornada na tecnologia em 2024, ao ingressar no
+                curso de Análise e Desenvolvimento de Sistemas, e no mesmo ano
+                conquistei meu primeiro estágio, onde embarquei no
+                desenvolvimento de software.
+              </p>
 
-                <p>
-                  Em 2025, atuei com marketing digital, trabalhando com ADS e
-                  criação de conteúdo para plataformas como Instagram, Facebook,
-                  Google e TikTok — experiência que hoje aplico para criar
-                  produtos que não apenas funcionam, mas convertem.
-                </p>
+              <p>
+                Em 2025, atuei com marketing digital, trabalhando com ADS e
+                criação de conteúdo para plataformas como Instagram, Facebook,
+                Google e TikTok — experiência que hoje aplico para criar
+                produtos que não apenas funcionam, mas convertem.
+              </p>
 
-                <p>
-                  Atualmente atuo como desenvolvedor freelancer , ajudando
-                  empresas e profissionais a irem além de apenas “ter um site”.
-                  Meu foco é construir páginas estratégicas, pensadas para
-                  atrair clientes, aumentar vendas e fortalecer marcas.
-                </p>
-              </div>
+              <p>
+                Atualmente atuo como desenvolvedor freelancer , ajudando
+                empresas e profissionais a irem além de apenas “ter um site”.
+                Meu foco é construir páginas estratégicas, pensadas para atrair
+                clientes, aumentar vendas e fortalecer marcas.
+              </p>
             </div>
           </div>
         </section>
         <section
           id="portifolio"
-          className="w-full h-screen flex flex-col gap-8 justify-center items-center bg-black snap-start"
+          className="w-full h-screen flex flex-col gap-8 justify-center items-center bg-linear-to-r from-gray-700 via-gray-900 to-black
+ snap-start"
         >
           <h2 className=" text-5xl font-bold text-white font-jet">
             Portifólio
@@ -178,7 +160,8 @@ export default function Home() {
         </section>
         <section
           id="habilidades"
-          className="w-full h-screen flex flex-col gap-8 justify-center items-center bg-black snap-start"
+          className="w-full h-screen flex flex-col gap-8 justify-center items-center bg-linear-to-r from-gray-700 via-gray-900 to-black
+ snap-start"
         >
           <h2 className=" font-jet font-black text-white text-5xl">
             Habilidades
@@ -189,7 +172,7 @@ export default function Home() {
         </section>
         <section
           id="contato"
-          className="not-even:w-full h-screen flex flex-col items-center bg-linear-to-t from-slate-950 to-lime-500 snap-start"
+          className="not-even:w-full h-screen flex flex-col items-center bg-linear-to-t from-slate-950 to-sky-500 snap-start"
         >
           <div className="flex-1 flex flex-col gap-10 items-center justify-center px-6 text-center">
             <h2 className="text-white text-6xl font-black font-jet">
